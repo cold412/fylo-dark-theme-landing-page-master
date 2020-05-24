9300ea34aa25a0d1b60814e7f1f6b148ca71c90d
cold412 <cold412+gitlab@protonmail.com>
2020-05-24 18:30:24 +0200

    beforeButton

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import MainHeader from '../pages/components/MainHeader'
+import MainText from '../pages/components/MainText'
 import '../styles/page1.scss'
 import mainbck from '../img/bg-curvy-desktop.svg'
 import mainImg from '../img/illustration-intro.png'
@@ -10,7 +11,8 @@ const Page1 = () => {
       <div className="page1Cont">
         <img src={mainImg} alt="MainImage"/>
         <MainHeader text="All your files in one secure location, accessible anywhere."/>
-        <img src={mainbck} alt="MainBackground"/>
+        <MainText text="Fylo stores all your most important files in one secure location. Access them wherever 
+  you need, share and collaborate with friends family, and co-workers."/>
       </div>
     )
 }
--- src/pages/components/MainText.js
+++ src/pages/components/MainText.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../../styles/maintext.scss'
 
 const MainText = (props) => {
     return (
