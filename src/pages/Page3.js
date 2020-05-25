1fe46ac624b8561259ef063c7156cf24952b3b59
cold412 <cold412+gitlab@protonmail.com>
2020-05-25 20:51:30 +0200

    page3

---
+++ src/pages/Page3.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import MainHeader from '../pages/components/MainHeader'
+import MainText from '../pages/components/MainText'
+import mainImg from '../img/illustration-stay-productive.png'
+import '../styles/page3.scss'
+
+function Page3() {
+    return (
+        <div className="page3__container">
+            <div className="page3__left">
+      <img src={mainImg} alt="MainImage" />
+            </div>
+            <div className="page3__right">
+
+      <MainHeader text="Stay productive, wherever you are" />
+      <MainText   text="Never let location be an issue when accessing your files. Fylo has you covered for all of your file 
+  storage needs." />
+      <MainText   text="Securely share files and folders with friends, family and colleagues for live collaboration. No email 
+  attachments required." />
+            </div>
+        </div>
+    )
+}
+
+export default Page3
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Navbar"
 import Page1 from "./Page1"
 import Page2 from "./Page2"
+import Page3 from "./Page3"
 import '../styles.scss'
 import '../styles/index.scss'
 
@@ -14,8 +15,11 @@ const index = () => {
       <Page1 />
       </div>
 
-      <div className="pate2Cont">
+      <div className="page2Cont">
       <Page2 />
+      </div>
+      <div className="page3Cont">
+      <Page3 />
       </div>
     </div>
   )
